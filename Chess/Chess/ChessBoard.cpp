64cedd0500d9fe7bd6d290eee3ad268f89d1c0c9
MonoCode22 <MonoCode22@gmail.com>
2020-12-10 20:06:58 +0200

    Initial ChessBoard commit

---
+++ Chess/Chess/ChessBoard.cpp
@@ -0,0 +1,116 @@
+#include "ChessBoard.h"
+
+/* ChessBoard Class */
+
+// Constructors:
+
+ChessBoard::ChessBoard(string startingBoard)
+{
+	// Inits:
+	int i = 0;
+
+	// Initializing the board array:
+	for (i = 0; i < BOARD_SIZE * BOARD_SIZE; i++)
+	{
+		// Condition: chess piece to create
+		if (startingBoard[i] != '#')
+		{
+			this->addPiece(startingBoard[i]);
+		}
+
+		// Condition: empty spot
+		else
+		{
+			this->_board.push_back(NULL);
+		}
+	}
+}
+
+
+// Destructors:
+
+ChessBoard::~ChessBoard()
+{
+	// Inits:
+	int i = 0, j = 0;
+
+	// Clearing the vector:
+	for (i = 0; i < BOARD_SIZE; i++)
+	{
+		// Condition: deleting the current chess piece
+		if (this->_board[i] != NULL)
+		{
+			delete this->_board[i];
+		}
+	}
+
+	this->_board.clear();
+}
+
+
+// Getters:
+
+std::vector<ChessPiece*> ChessBoard::getBoard() const
+{
+	return this->_board;
+}
+
+
+// Setters:
+
+void ChessBoard::setBoard(BoardPosition src, BoardPosition dest)
+{
+	// Switching both positions:
+	this->_board[dest.getRow() * BOARD_SIZE + dest.getColumn()] = this->_board[src.getRow() * BOARD_SIZE + src.getColumn()];
+	
+	// Emptying the source position:
+	this->_board[src.getRow() * BOARD_SIZE + src.getRow()] = NULL;
+}
+
+
+// Methods:
+
+string ChessBoard::toString() const
+{
+	// Inits:
+	string boardString = "";
+	int i = 0;
+
+	// Building the board string:
+	for (i = 0; i < this->_board.size(); i++)
+	{
+		// Condition: chess piece
+		if (this->_board[i] != NULL)
+		{
+			boardString += this->_board[i]->getPieceType();
+		}
+
+		// Condition: empty spot
+		else
+		{
+			boardString += EMPTY_SPACE;
+		}
+	}
+
+	// Starting player:
+	boardString += STARTING_PLAYER;
+
+	return boardString;
+}
+
+
+// Helper Methods:
+
+void ChessBoard::addPiece(char pieceType)
+{
+	// Creating the proper chess piece:
+	switch (toupper(pieceType))
+	{
+		case 'P': this->_board.push_back(new Pawn(pieceType)); break;
+		case 'N': this->_board.push_back(new Knight(pieceType)); break;
+		case 'B': this->_board.push_back(new Bishop(pieceType)); break;
+		case 'R': this->_board.push_back(new Rook(pieceType)); break;
+		case 'Q': this->_board.push_back(new Queen(pieceType)); break;
+		case 'K': this->_board.push_back(new King(pieceType)); break;
+	}
+}
---
+++ Chess/Chess/ChessBoard.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+#include "BoardPosition.h"
+#include "ChessPiece.h"
+
+using std::string;
+
+#define BOARD_SIZE 8
+
+#define EMPTY_SPACE '#'
+#define STARTING_PLAYER '0'
+
+class ChessPiece;
+
+class ChessBoard
+{
+public:
+	// Constructors:
+	ChessBoard(string startingBoard);
+
+	// Deconstructors:
+	~ChessBoard();
+
+	// Getters:
+	std::vector<ChessPiece*> getBoard() const;
+
+	// Setters:
+	void setBoard(BoardPosition src, BoardPosition dest);
+	
+	// Methods:
+	string toString() const;
+
+private:
+	// Fields:
+	std::vector<ChessPiece*> _board;
+
+	// Helper Methods:
+	void addPiece(char pieceType);
+};
