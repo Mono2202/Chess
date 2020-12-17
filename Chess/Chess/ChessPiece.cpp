e0436dd8135683d38a5904998a914558c75fee94
MonoCode22 <MonoCode22@gmail.com>
2020-12-17 19:43:00 +0200

    Fixed index calculation

--- Chess/Chess/ChessPiece.cpp
+++ Chess/Chess/ChessPiece.cpp
@@ -25,12 +25,12 @@ char ChessPiece::getPieceType() const
 
 // Protected Methods:
 
-string ChessPiece::generalMoveCheck(string srcPos, string destPos, std::vector<ChessPiece*> board)
+string ChessPiece::generalMoveCheck(string srcPos, string destPos, std::vector<ChessPiece*> board) // TODO: ADD CURRENT PLAYER PARAMETER
 {
 	// Inits:
 	string returnCode = "0";
-	int srcIndex = srcPos[0] - 'a' + (BOARD_SIZE - srcPos[1]) * BOARD_SIZE;
-	int destIndex = destPos[0] - 'a' + (BOARD_SIZE - destPos[1]) * BOARD_SIZE;
+	int srcIndex = srcPos[0] - 'a' + (BOARD_SIZE - (srcPos[1] - '0')) * BOARD_SIZE;
+	int destIndex = destPos[0] - 'a' + (BOARD_SIZE - (destPos[1] - '0')) * BOARD_SIZE;
 
 	// TODO: #define
 	// Condition: move index out of range (Move Code: 5)
@@ -38,20 +38,21 @@ string ChessPiece::generalMoveCheck(string srcPos, string destPos, std::vector<C
 		(srcPos[1] < '1' || srcPos[1] > '8') ||
 		(destPos[0] < 'a' || destPos[0] > 'h') ||
 		(destPos[1] < '1' || destPos[1] > '8'))
-		returnCode = MoveCodes::ToString(MoveCodes::ERROR_INVALID_INDEX);
+		returnCode = MoveCodes::ToString(MoveCodes::CODES::ERROR_INVALID_PIECE_INDEX);
 
 	// Condition: srcPos equals to the destPos (Move Code: 7)
 	else if (srcIndex == destIndex)
-		returnCode = MoveCodes::ToString(MoveCodes::ERROR_SRC_IS_DEST);
+		returnCode = MoveCodes::ToString(MoveCodes::CODES::ERROR_SRC_IS_DEST);
 
 	// Condition: no friendly piece in srcPos (Move Code: 2)
 	else if (board[srcIndex] == NULL || 
 			 islower(this->getPieceType()) != islower(board[srcIndex]->getPieceType()))
-		returnCode = MoveCodes::ToString(MoveCodes::ERROR_NO_FRIENDLY_PIECE_IN_SRC);
+		returnCode = MoveCodes::ToString(MoveCodes::CODES::ERROR_NO_FRIENDLY_PIECE_IN_SRC);
 
 	// Condition: friendly piece in destPos (Move Code: 3)
-	else if (islower(this->getPieceType()) == islower(board[destIndex]->getPieceType()))
-		returnCode = MoveCodes::ToString(MoveCodes::ERROR_FRIENDLY_PIECE_IN_DEST);
+	else if (board[destIndex] != NULL &&
+			 islower(this->getPieceType()) == islower(board[destIndex]->getPieceType()))
+		returnCode = MoveCodes::ToString(MoveCodes::CODES::ERROR_FRIENDLY_PIECE_IN_DEST);
 
 	// Condition: move will lead to check on the friendly King (Move Code: 4)
 	else if (false)
