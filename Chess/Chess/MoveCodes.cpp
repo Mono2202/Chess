609314e3337a769c72b167e71f4998d3b50d9651
MonoCode22 <MonoCode22@gmail.com>
2020-12-17 19:41:18 +0200

    Fixed enum bugs

--- Chess/Chess/MoveCodes.cpp
+++ Chess/Chess/MoveCodes.cpp
@@ -13,7 +13,7 @@ code - the move code
 Output:
 the code as a string
 */
-string ToString(int code)
+string MoveCodes::ToString(int code)
 {
 	switch (code)
 	{
@@ -22,7 +22,7 @@ string ToString(int code)
 		case MoveCodes::ERROR_NO_FRIENDLY_PIECE_IN_SRC: return "2";
 		case MoveCodes::ERROR_FRIENDLY_PIECE_IN_DEST:	return "3";
 		case MoveCodes::ERROR_FRIENDLY_CHECK:			return "4";
-		case MoveCodes::ERROR_INVALID_INDEX:			return "5";
+		case MoveCodes::ERROR_INVALID_PIECE_INDEX:		return "5";
 		case MoveCodes::ERROR_INVALID_MOVE:				return "6";
 		case MoveCodes::ERROR_SRC_IS_DEST:				return "7";
 		case MoveCodes::VALID_CHECKMATE:				return "8";
