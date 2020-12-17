bbbbe8086d449153b90d7e15204687c1dc058ca5
MonoCode22 <MonoCode22@gmail.com>
2020-12-17 16:09:20 +0200

    Initial Rook commit

---
+++ Chess/Chess/Rook.cpp
@@ -0,0 +1,20 @@
+#include "Rook.h"
+
+/* Rook Class */
+
+// Constructors:
+
+Rook::Rook(char pieceType, int position) : ChessPiece(pieceType, position) { }
+
+
+// Destructors:
+
+Rook::~Rook() { }
+
+
+// Virtual Functions:
+
+string Rook::move(string directions)
+{
+
+}
---
+++ Chess/Chess/Rook.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ChessPiece.h"
+
+class Rook : public ChessPiece
+{
+public:
+	// Constructors:
+	Rook(char pieceType, int position);
+	
+	// Deconstructors:
+	~Rook();
+
+	// Virtual Functions:
+	virtual string move(string directions);
+};
