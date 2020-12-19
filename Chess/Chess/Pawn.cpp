48e7135907e578fd39402fdeacedf5725410ef60
Yoni Shifrin <yonishifrin@gmail.com>
2020-12-19 17:00:12 +0200

    Finished Pawn's move function

---
+++ Chess/Chess/Pawn.cpp
@@ -0,0 +1,44 @@
+#include "Pawn.h"
+
+/* Pawn Class */
+
+// Constructors:
+
+Pawn::Pawn(char pieceType) : ChessPiece(pieceType) { this->_hasMoved = false; }
+
+
+// Destructors:
+
+Pawn::~Pawn() { this->_hasMoved = false; }
+
+
+// Virtual Functions:
+
+bool Pawn::move(BoardPosition srcPos, BoardPosition destPos, ChessPiece* board[BOARD_SIZE][BOARD_SIZE], bool isWhite)
+{
+	// Inits:
+	int moveDirection = (isWhite) ? -1 : 1;
+	bool isValidMove = false;
+	int row = 0, column = 0;
+
+	// Condition: first Pawn move
+	if (!this->_hasMoved && (destPos.getRow() - srcPos.getRow()) * moveDirection <= 2 &&
+		srcPos.getColumn() == destPos.getColumn() &&
+		board[destPos.getRow()][destPos.getColumn()] == NULL &&
+		board[srcPos.getRow() + moveDirection][srcPos.getColumn()] == NULL)
+	{
+		isValidMove = true;
+		this->_hasMoved = true;
+	}
+	
+	// Condition: not first Pawn move
+	else if (this->_hasMoved && (destPos.getRow() - srcPos.getRow()) * moveDirection <= 1 && srcPos.getColumn() == destPos.getColumn() && board[destPos.getRow()][destPos.getColumn()] == NULL)
+		isValidMove = true;
+
+	// Condition: Pawn is able to attack
+	else if (abs(destPos.getRow() - srcPos.getRow()) == 1 && abs(destPos.getColumn() - srcPos.getColumn()) == 1 && board[destPos.getRow()][destPos.getColumn()] != NULL)
+		isValidMove = true;
+
+	// Condition: invalid Pawn move (Move Code: 6)
+	return isValidMove;
+}
--- Chess/Chess/Pawn.h
+++ Chess/Chess/Pawn.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "ChessPiece.h"
+
+class Pawn : public ChessPiece
+{
+public:
+	// Constructors:
+	Pawn(char pieceType);
+	
+	// Deconstructors:
+	~Pawn();
+
+	// Virtual Functions:
+	virtual bool move(BoardPosition srcPos, BoardPosition destPos, ChessPiece* board[BOARD_SIZE][BOARD_SIZE], bool isWhite);
+
+private:
+	// Fields:
+	bool _hasMoved;
+};
